5d535f5c90479615c2c447f84510aa6425bf0b62
jhon <blueacademy99@gmail.com>
2024-08-29 20:40:03 -0500

    modificación 1

--- enums/aplication.java
+++ enums/aplication.java
@@ -12,6 +12,22 @@ public class aplication {
     // obtenemos valores o atributos mediante los metodos creados :
     System.out.println(" mostrando valores mediante sus metodos :");
     System.out.println(dia.getDescripcion());
+
+    // acceder a las constantes del Enum turno  Turno
+    System.out.println(" accediendo a las constantes del Enum Turno");
+    System.out.println(turno);
+    // accediendo a las constantes del enum DiaSemana
+    System.out.println(" accediante a la constante del enum dia :");
+    System.out.println(dia);
+
+    // obteniendo todos los valores del enum
+
+    DiaSemana []dias =DiaSemana.values();
+    System.out.println(" mostrando la descripcion de todos los dias de la semana ");
+     for (int i = 0; i < dias.length; i++) {
+        System.out.println( dias[i].getDescripcion());
+        
+     }
     
   }
   
