f998e6e6473a03bd6afba1b92f361f0be797051a
jhon <blueacademy99@gmail.com>
2024-08-29 20:48:31 -0500

    modificación 2

--- enums/aplication.java
+++ enums/aplication.java
@@ -4,6 +4,7 @@ import otros.DiaSemana;
 
 public class aplication {
   public static void main(String[] args) {
+    System.out.println(" \n ");
     Turno turno=Turno.MAÑANA;
     System.out.println(" el turno creado es :"+turno);
 
@@ -21,12 +22,20 @@ public class aplication {
     System.out.println(dia);
 
     // obteniendo todos los valores del enum
-
+    System.out.println(" \n ");
     DiaSemana []dias =DiaSemana.values();
     System.out.println(" mostrando la descripcion de todos los dias de la semana ");
      for (int i = 0; i < dias.length; i++) {
         System.out.println( dias[i].getDescripcion());
         
+     }
+
+     // comparando los valors de enum 
+     System.out.println(" \n ");
+     System.out.println(" comparando si es lunes de el (enum) con la instancia que tenemos llamado (dia) ");
+
+     if(dia==DiaSemana.LUNES){
+        System.out.println(" es correcto hoy es lunes "+dia.getDescripcion());
      }
     
   }
